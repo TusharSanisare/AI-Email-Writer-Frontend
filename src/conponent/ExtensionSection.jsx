a4f7352930fb6baa04da596dc7b56559ac36d9c5
TusharSanisare <tsanisare@gmail.com>
2025-01-20 03:06:56 +0530

    done

--- src/conponent/ExtensionSection.jsx
+++ src/conponent/ExtensionSection.jsx
@@ -1,20 +1,18 @@
-import React from "react";
+import extImg from "../assets/images/ext-img.png";
 
 const ExtensionSection = () => {
   return (
-    <>
-      <div class="extention-img-section">
-        <h1 class="extension-title">AI Email Writer Chrome Extension</h1>
-        <img class="ext-img" src="../src/assets/images/ext-img.png" alt="" />
-        <a
-          target="_blank"
-          href="https://github.com/TusharSanisare/AI-Email-Writer-Chrome-Extension"
-          class="extension-btn"
-        >
-          Download Extension
-        </a>
-      </div>
-    </>
+    <div className="extention-img-section">
+      <h1 className="extension-title">AI Email Writer Chrome Extension</h1>
+      <img className="ext-img" src={extImg} alt="Extension Screenshot" />
+      <a
+        target="_blank"
+        href="https://github.com/TusharSanisare/AI-Email-Writer-Chrome-Extension"
+        className="extension-btn"
+      >
+        Download Extension
+      </a>
+    </div>
   );
 };
 
